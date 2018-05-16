cfd8df021d659869c4c3970bc2cf1bd4929febee
gonzalezroberto <robertogonzalez0513@gmail.com>
2018-05-16 09:52:34 -0700

    afaf

--- src/Components/News.js
+++ src/Components/News.js
@@ -1,9 +1,12 @@
 import React from 'react';
-
+import "../stylesheets/News.css"
 const News = (props) => {
 return (
 <li>
 <div className="comicbook-element">
+<div className ="coverart" />
+ <img src={props.news.coverart} width = "10" height = "20"/>
+
 <div className="date">
 date: {props.news.dateposted}
 </div>
