dd43d2afc9838b8225ee715d7200d7732542a2be
gonzalezroberto <robertogonzalez0513@gmail.com>
2018-05-16 17:30:32 -0700

    lowd

--- src/App.js
+++ src/App.js
@@ -8,84 +8,42 @@ import Timeline from './Components/Timeline';
 import "./App.css"
 import React from 'react'
 import { BrowserRouter as Router, Route,Switch, Link, redirect, withRouter} from 'react-router-dom'
-{
-  this.state = {redirect: ''}
-}
 class Main extends React.Component {
   constructor() {
     super();
     this.state = {redirect:''};
   }
   componentWillMount(){
-    console.log("in componentDidMount")
-    fetch('api/login', {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    credentials: "include"
-  }).then(res => res.json()).then( json =>
-    {
-      console.log('json', json);
-      this.setState({redirect:json});
-    })
-  }
-  switchView(props){
-    if(props.state.redirect === false)
-    return Mainlog();
-    else {
-      return loggedinMain();
-    }
+    console.log("in componentWillMount")
+    fetch('api/login')
+    .then(res => res.json()).then( json =>{console.log('json', json);this.setState({redirect:json});})
+    console.log("leaving componentWillMount")
   }
   render(){
-    if(this.state.redirect === false)
-      return (Mainlog());
-    else {
-      return (loggedinMain());
-    }
-
-  }
-}
-
-const Mainlog = () => (
-        <Router>
-            <div className ="main-background">
-              <header>
-                <div className = "top_header"></div>
-              <nav>
-              <ul>
-                  <a href="/">Home </a>
-                  <a href="/search">Search </a>
-                  <a href="/login">Login/Signup </a>
-              </ul>
-              </nav>
-              </header>
-              <Route exact path="/" render={() => <News/>}/>
-              <Route  path="/search" render={() => <Searchbar/>}/>
-              <Route exact path="/register" render={() =><Signup/>}/>
-              <Route exact path="/login" render={() => <Login/>}/>
-          </div>
-        </Router>
+    return(
+      <div>
+      <Router>
+          <div className ="main-background">
+            <header>
+              <div className = "top_header"></div>
+            <nav>
+            <ul>
+                <Link to="/">Home </Link>
+                <Link to="/search">Search </Link>
+                <Link to="/profile">Profile </Link>
+                <a href="/login">Login/Signup </a>
+            </ul>
+            </nav>
+            </header>
+            <Route exact path="/" render={() => <News/>}/>
+            <Route  path="/search" render={() => <Searchbar/>}/>
+            <Route exact path="/register" render={() =><Signup/>}/>
+            <Route exact path="/login" render={() => <Login/>}/>
+            <Route exact path="/profile" render={() => <Timeline/>}/>
+        </div>
+      </Router>
+      </div>
     );
-    const loggedinMain = () => (
-            <Router>
-                <div className ="main-background">
-                  <header>
-                    <div className = "top_header"></div>
-                  <nav>
-                  <ul>
-                      <Link to="/">Home </Link>
-                      <Link to="/search">Search </Link>
-                      <Link to="/profile">Profile </Link>
-                  </ul>
-                  </nav>
-                  </header>
-                  <Route exact path="/" render={() => <News/>}/>
-                  <Route  path="/search" render={() => <Searchbar/>}/>
-                  <Route exact path="/register" render={() =><Signup/>}/>
-                  <Route exact path="/login" render={() => <Login/>}/>
-                  <Route exact path="/profile" render={() => <Timeline/>}/>
-              </div>
-            </Router>
-        );
-export default Main;
+    }
+  }
+  export default withRouter(Main);
